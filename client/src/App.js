e9329f6b1a194a2b862915b05fff4f86f98ed5ec
Osman <ojan1@my.centennialcollege.ca>
2022-05-07 15:38:16 -0400

    added welcome landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Welcome from './components/Welcome/Welcome';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Welcome />
     </div>
   );
 }
